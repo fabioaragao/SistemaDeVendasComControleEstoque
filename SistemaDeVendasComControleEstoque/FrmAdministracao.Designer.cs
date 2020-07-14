ce78000ebd623288ef5adfcd3c4dd45b263c9e83
cml11 <cml11@CML11>
2020-07-14 15:32:23 -0300

    ajustes finos iniciais

--- SistemaDeVendasComControleEstoque/FrmAdministracao.Designer.cs
+++ SistemaDeVendasComControleEstoque/FrmAdministracao.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.msFrmAdminSistema = new System.Windows.Forms.MenuStrip();
-            this.usuárioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.vendasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.estoqueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -40,33 +40,39 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblBarraInferior = new System.Windows.Forms.Label();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroDeProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inativarProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.suporteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.msFrmAdminSistema.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // msFrmAdminSistema
             // 
+            this.msFrmAdminSistema.BackColor = System.Drawing.SystemColors.ControlLight;
             this.msFrmAdminSistema.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.usuárioToolStripMenuItem,
+            this.usuarioToolStripMenuItem,
             this.vendasToolStripMenuItem,
             this.estoqueToolStripMenuItem,
             this.sistemaToolStripMenuItem,
             this.sairToolStripMenuItem});
             this.msFrmAdminSistema.Location = new System.Drawing.Point(0, 0);
             this.msFrmAdminSistema.Name = "msFrmAdminSistema";
-            this.msFrmAdminSistema.Size = new System.Drawing.Size(800, 24);
+            this.msFrmAdminSistema.Size = new System.Drawing.Size(854, 24);
             this.msFrmAdminSistema.TabIndex = 0;
             this.msFrmAdminSistema.Text = "menuStrip1";
             // 
-            // usuárioToolStripMenuItem
+            // usuarioToolStripMenuItem
             // 
-            this.usuárioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuarioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.cadastroDeUsuáriosToolStripMenuItem,
             this.editarUsuárioToolStripMenuItem,
             this.exclusãoToolStripMenuItem});
-            this.usuárioToolStripMenuItem.Name = "usuárioToolStripMenuItem";
-            this.usuárioToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
-            this.usuárioToolStripMenuItem.Text = "Usuário";
+            this.usuarioToolStripMenuItem.Name = "usuarioToolStripMenuItem";
+            this.usuarioToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.usuarioToolStripMenuItem.Text = "Usuário";
             // 
             // vendasToolStripMenuItem
             // 
@@ -78,12 +84,19 @@
             // 
             // estoqueToolStripMenuItem
             // 
+            this.estoqueToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroDeProdutosToolStripMenuItem,
+            this.editarProdutosToolStripMenuItem,
+            this.inativarProdutosToolStripMenuItem});
             this.estoqueToolStripMenuItem.Name = "estoqueToolStripMenuItem";
             this.estoqueToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.estoqueToolStripMenuItem.Text = "Estoque";
             // 
             // sistemaToolStripMenuItem
             // 
+            this.sistemaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backupToolStripMenuItem,
+            this.suporteToolStripMenuItem});
             this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
             this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.sistemaToolStripMenuItem.Text = "Sistema";
@@ -92,19 +105,19 @@
             // 
             this.cadastroDeUsuáriosToolStripMenuItem.Name = "cadastroDeUsuáriosToolStripMenuItem";
             this.cadastroDeUsuáriosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.cadastroDeUsuáriosToolStripMenuItem.Text = "Cadastro";
+            this.cadastroDeUsuáriosToolStripMenuItem.Text = "Cadastrar";
             // 
             // editarUsuárioToolStripMenuItem
             // 
             this.editarUsuárioToolStripMenuItem.Name = "editarUsuárioToolStripMenuItem";
             this.editarUsuárioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.editarUsuárioToolStripMenuItem.Text = "Alteração";
+            this.editarUsuárioToolStripMenuItem.Text = "Alterar";
             // 
             // exclusãoToolStripMenuItem
             // 
             this.exclusãoToolStripMenuItem.Name = "exclusãoToolStripMenuItem";
             this.exclusãoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exclusãoToolStripMenuItem.Text = "Exclusão";
+            this.exclusãoToolStripMenuItem.Text = "Inativar";
             // 
             // abrirSistemaDeVendasToolStripMenuItem
             // 
@@ -117,9 +130,9 @@
             this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
             this.panel1.Controls.Add(this.lblBarraInferior);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 422);
+            this.panel1.Location = new System.Drawing.Point(0, 463);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 28);
+            this.panel1.Size = new System.Drawing.Size(854, 28);
             this.panel1.TabIndex = 1;
             // 
             // lblBarraInferior
@@ -128,7 +141,7 @@
             this.lblBarraInferior.AutoSize = true;
             this.lblBarraInferior.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblBarraInferior.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblBarraInferior.Location = new System.Drawing.Point(199, 0);
+            this.lblBarraInferior.Location = new System.Drawing.Point(226, 4);
             this.lblBarraInferior.Name = "lblBarraInferior";
             this.lblBarraInferior.Size = new System.Drawing.Size(395, 20);
             this.lblBarraInferior.TabIndex = 0;
@@ -140,11 +153,41 @@
             this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
             this.sairToolStripMenuItem.Text = "Sair";
             // 
+            // cadastroDeProdutosToolStripMenuItem
+            // 
+            this.cadastroDeProdutosToolStripMenuItem.Name = "cadastroDeProdutosToolStripMenuItem";
+            this.cadastroDeProdutosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cadastroDeProdutosToolStripMenuItem.Text = "Cadastrar Produtos";
+            // 
+            // editarProdutosToolStripMenuItem
+            // 
+            this.editarProdutosToolStripMenuItem.Name = "editarProdutosToolStripMenuItem";
+            this.editarProdutosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editarProdutosToolStripMenuItem.Text = "Editar Produtos";
+            // 
+            // inativarProdutosToolStripMenuItem
+            // 
+            this.inativarProdutosToolStripMenuItem.Name = "inativarProdutosToolStripMenuItem";
+            this.inativarProdutosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inativarProdutosToolStripMenuItem.Text = "Inativar Produtos";
+            // 
+            // backupToolStripMenuItem
+            // 
+            this.backupToolStripMenuItem.Name = "backupToolStripMenuItem";
+            this.backupToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.backupToolStripMenuItem.Text = "Backup";
+            // 
+            // suporteToolStripMenuItem
+            // 
+            this.suporteToolStripMenuItem.Name = "suporteToolStripMenuItem";
+            this.suporteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.suporteToolStripMenuItem.Text = "Suporte";
+            // 
             // FrmAdministracao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(854, 491);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.msFrmAdminSistema);
             this.MainMenuStrip = this.msFrmAdminSistema;
@@ -164,7 +207,7 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip msFrmAdminSistema;
-        private System.Windows.Forms.ToolStripMenuItem usuárioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuarioToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem vendasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem estoqueToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
@@ -175,6 +218,11 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblBarraInferior;
         private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroDeProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inativarProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem suporteToolStripMenuItem;
     }
 }
 
