e54b87f804812f39730679fe73f527f036051d5e
Fábio Aragao <fabio_aragao@yahoo.com.br>
2020-07-15 03:48:30 -0300

    limpeza no projeto

--- SistemaDeVendasComControleEstoque/Formularios/FrmCadastroUsuario.Designer.cs
+++ SistemaDeVendasComControleEstoque/Formularios/FrmCadastroUsuario.Designer.cs
@@ -28,70 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblNomeCompleto = new System.Windows.Forms.Label();
-            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
-            this.txtEndereço = new System.Windows.Forms.TextBox();
-            this.lblEndereço = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // lblNomeCompleto
-            // 
-            this.lblNomeCompleto.AutoSize = true;
-            this.lblNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNomeCompleto.Location = new System.Drawing.Point(12, 9);
-            this.lblNomeCompleto.Name = "lblNomeCompleto";
-            this.lblNomeCompleto.Size = new System.Drawing.Size(122, 18);
-            this.lblNomeCompleto.TabIndex = 0;
-            this.lblNomeCompleto.Text = "Nome Completo:";
-            // 
-            // txtNomeCompleto
-            // 
-            this.txtNomeCompleto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNomeCompleto.Location = new System.Drawing.Point(15, 30);
-            this.txtNomeCompleto.Name = "txtNomeCompleto";
-            this.txtNomeCompleto.Size = new System.Drawing.Size(339, 22);
-            this.txtNomeCompleto.TabIndex = 1;
-            // 
-            // txtEndereço
-            // 
-            this.txtEndereço.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtEndereço.Location = new System.Drawing.Point(15, 76);
-            this.txtEndereço.Name = "txtEndereço";
-            this.txtEndereço.Size = new System.Drawing.Size(339, 22);
-            this.txtEndereço.TabIndex = 3;
-            // 
-            // lblEndereço
-            // 
-            this.lblEndereço.AutoSize = true;
-            this.lblEndereço.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEndereço.Location = new System.Drawing.Point(12, 55);
-            this.lblEndereço.Name = "lblEndereço";
-            this.lblEndereço.Size = new System.Drawing.Size(76, 18);
-            this.lblEndereço.TabIndex = 2;
-            this.lblEndereço.Text = "Endereço:";
             // 
             // FrmCadastroUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtEndereço);
-            this.Controls.Add(this.lblEndereço);
-            this.Controls.Add(this.txtNomeCompleto);
-            this.Controls.Add(this.lblNomeCompleto);
             this.Name = "FrmCadastroUsuario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Cadastro de Usuários do Sistema";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblNomeCompleto;
-        private System.Windows.Forms.TextBox txtNomeCompleto;
-        private System.Windows.Forms.TextBox txtEndereço;
-        private System.Windows.Forms.Label lblEndereço;
     }
 }
