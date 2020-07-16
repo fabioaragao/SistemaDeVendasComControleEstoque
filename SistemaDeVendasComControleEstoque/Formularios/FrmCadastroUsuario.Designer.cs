7ac23c685b682d6985353dda7b1c0696876a725a
cml11 <cml11@CML11>
2020-07-16 14:10:36 -0300

    Inseridas algumas funções na tela de cadastro de usuários

--- SistemaDeVendasComControleEstoque/Formularios/FrmCadastroUsuario.Designer.cs
+++ SistemaDeVendasComControleEstoque/Formularios/FrmCadastroUsuario.Designer.cs
@@ -30,6 +30,16 @@
         {
             this.lblNomeCompleto = new System.Windows.Forms.Label();
             this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtRepitaSenha = new System.Windows.Forms.TextBox();
+            this.lblRepitaSenha = new System.Windows.Forms.Label();
+            this.lblNivelAcesso = new System.Windows.Forms.Label();
+            this.cbNivelAcesso = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // lblNomeCompleto
@@ -50,11 +60,119 @@
             this.txtNomeCompleto.Size = new System.Drawing.Size(369, 22);
             this.txtNomeCompleto.TabIndex = 1;
             // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(12, 58);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(45, 16);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(15, 77);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(369, 22);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogin.Location = new System.Drawing.Point(15, 132);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(180, 22);
+            this.txtLogin.TabIndex = 5;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(12, 113);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(44, 16);
+            this.lblLogin.TabIndex = 4;
+            this.lblLogin.Text = "Login:";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(15, 186);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(180, 22);
+            this.txtSenha.TabIndex = 9;
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(12, 167);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(50, 16);
+            this.lblSenha.TabIndex = 8;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // txtRepitaSenha
+            // 
+            this.txtRepitaSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRepitaSenha.Location = new System.Drawing.Point(204, 186);
+            this.txtRepitaSenha.Name = "txtRepitaSenha";
+            this.txtRepitaSenha.PasswordChar = '*';
+            this.txtRepitaSenha.Size = new System.Drawing.Size(180, 22);
+            this.txtRepitaSenha.TabIndex = 11;
+            // 
+            // lblRepitaSenha
+            // 
+            this.lblRepitaSenha.AutoSize = true;
+            this.lblRepitaSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRepitaSenha.Location = new System.Drawing.Point(201, 167);
+            this.lblRepitaSenha.Name = "lblRepitaSenha";
+            this.lblRepitaSenha.Size = new System.Drawing.Size(104, 16);
+            this.lblRepitaSenha.TabIndex = 10;
+            this.lblRepitaSenha.Text = "Repita a Senha:";
+            // 
+            // lblNivelAcesso
+            // 
+            this.lblNivelAcesso.AutoSize = true;
+            this.lblNivelAcesso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNivelAcesso.Location = new System.Drawing.Point(201, 113);
+            this.lblNivelAcesso.Name = "lblNivelAcesso";
+            this.lblNivelAcesso.Size = new System.Drawing.Size(91, 16);
+            this.lblNivelAcesso.TabIndex = 6;
+            this.lblNivelAcesso.Text = "Nivel Acesso:";
+            // 
+            // cbNivelAcesso
+            // 
+            this.cbNivelAcesso.FormattingEnabled = true;
+            this.cbNivelAcesso.Items.AddRange(new object[] {
+            "",
+            "Administrador",
+            "Balcão",
+            "Caixa",
+            "Estoque"});
+            this.cbNivelAcesso.Location = new System.Drawing.Point(204, 132);
+            this.cbNivelAcesso.Name = "cbNivelAcesso";
+            this.cbNivelAcesso.Size = new System.Drawing.Size(180, 21);
+            this.cbNivelAcesso.TabIndex = 7;
+            // 
             // FrmCadastroUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(431, 405);
+            this.ClientSize = new System.Drawing.Size(402, 405);
+            this.Controls.Add(this.cbNivelAcesso);
+            this.Controls.Add(this.lblNivelAcesso);
+            this.Controls.Add(this.txtRepitaSenha);
+            this.Controls.Add(this.lblRepitaSenha);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.txtNomeCompleto);
             this.Controls.Add(this.lblNomeCompleto);
             this.Name = "FrmCadastroUsuario";
@@ -69,5 +187,15 @@
 
         private System.Windows.Forms.Label lblNomeCompleto;
         private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtRepitaSenha;
+        private System.Windows.Forms.Label lblRepitaSenha;
+        private System.Windows.Forms.Label lblNivelAcesso;
+        private System.Windows.Forms.ComboBox cbNivelAcesso;
     }
 }
