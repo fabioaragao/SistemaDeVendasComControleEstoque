3c33b3a524b9323be870df7d2c946fca4de00b54
Fábio Aragao <fabio_aragao@yahoo.com.br>
2020-07-23 19:21:17 -0300

    Formulario Cadastro de Produtos

--- SistemaDeVendasComControleEstoque/Formularios/FrmCadastroProdutos.Designer.cs
+++ SistemaDeVendasComControleEstoque/Formularios/FrmCadastroProdutos.Designer.cs
@@ -30,8 +30,18 @@
         {
             this.lblCodProduto = new System.Windows.Forms.Label();
             this.txtCodProduto = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblNomeProduto = new System.Windows.Forms.Label();
+            this.txtNomeProduto = new System.Windows.Forms.TextBox();
+            this.txtCodigoBarras = new System.Windows.Forms.TextBox();
+            this.lblCodBarras = new System.Windows.Forms.Label();
+            this.lblFornecedor = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblPrecoCusto = new System.Windows.Forms.Label();
+            this.txtPrecoCusto = new System.Windows.Forms.TextBox();
+            this.txtPrecoVenda = new System.Windows.Forms.TextBox();
+            this.lblPrecoVenda = new System.Windows.Forms.Label();
+            this.txtLucro = new System.Windows.Forms.TextBox();
+            this.lblLucro = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblCodProduto
@@ -53,31 +63,128 @@
             this.txtCodProduto.TabIndex = 1;
             this.txtCodProduto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // label1
+            // lblNomeProduto
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(224, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Nome do Produto:";
+            this.lblNomeProduto.AutoSize = true;
+            this.lblNomeProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeProduto.Location = new System.Drawing.Point(224, 9);
+            this.lblNomeProduto.Name = "lblNomeProduto";
+            this.lblNomeProduto.Size = new System.Drawing.Size(117, 16);
+            this.lblNomeProduto.TabIndex = 2;
+            this.lblNomeProduto.Text = "Nome do Produto:";
             // 
-            // textBox1
+            // txtNomeProduto
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(347, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(319, 22);
-            this.textBox1.TabIndex = 3;
+            this.txtNomeProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeProduto.Location = new System.Drawing.Point(347, 6);
+            this.txtNomeProduto.Name = "txtNomeProduto";
+            this.txtNomeProduto.Size = new System.Drawing.Size(323, 22);
+            this.txtNomeProduto.TabIndex = 3;
+            // 
+            // txtCodigoBarras
+            // 
+            this.txtCodigoBarras.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoBarras.Location = new System.Drawing.Point(135, 34);
+            this.txtCodigoBarras.Name = "txtCodigoBarras";
+            this.txtCodigoBarras.Size = new System.Drawing.Size(206, 22);
+            this.txtCodigoBarras.TabIndex = 5;
+            // 
+            // lblCodBarras
+            // 
+            this.lblCodBarras.AutoSize = true;
+            this.lblCodBarras.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodBarras.Location = new System.Drawing.Point(12, 37);
+            this.lblCodBarras.Name = "lblCodBarras";
+            this.lblCodBarras.Size = new System.Drawing.Size(117, 16);
+            this.lblCodBarras.TabIndex = 4;
+            this.lblCodBarras.Text = "Código de Barras:";
+            // 
+            // lblFornecedor
+            // 
+            this.lblFornecedor.AutoSize = true;
+            this.lblFornecedor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFornecedor.Location = new System.Drawing.Point(347, 37);
+            this.lblFornecedor.Name = "lblFornecedor";
+            this.lblFornecedor.Size = new System.Drawing.Size(81, 16);
+            this.lblFornecedor.TabIndex = 6;
+            this.lblFornecedor.Text = "Fornecedor:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(434, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(236, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // lblPrecoCusto
+            // 
+            this.lblPrecoCusto.AutoSize = true;
+            this.lblPrecoCusto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecoCusto.Location = new System.Drawing.Point(12, 66);
+            this.lblPrecoCusto.Name = "lblPrecoCusto";
+            this.lblPrecoCusto.Size = new System.Drawing.Size(103, 16);
+            this.lblPrecoCusto.TabIndex = 8;
+            this.lblPrecoCusto.Text = "Preço de Custo:";
+            // 
+            // txtPrecoCusto
+            // 
+            this.txtPrecoCusto.Location = new System.Drawing.Point(121, 65);
+            this.txtPrecoCusto.Name = "txtPrecoCusto";
+            this.txtPrecoCusto.Size = new System.Drawing.Size(116, 20);
+            this.txtPrecoCusto.TabIndex = 9;
+            // 
+            // txtPrecoVenda
+            // 
+            this.txtPrecoVenda.Location = new System.Drawing.Point(358, 65);
+            this.txtPrecoVenda.Name = "txtPrecoVenda";
+            this.txtPrecoVenda.Size = new System.Drawing.Size(128, 20);
+            this.txtPrecoVenda.TabIndex = 11;
+            // 
+            // lblPrecoVenda
+            // 
+            this.lblPrecoVenda.AutoSize = true;
+            this.lblPrecoVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecoVenda.Location = new System.Drawing.Point(243, 66);
+            this.lblPrecoVenda.Name = "lblPrecoVenda";
+            this.lblPrecoVenda.Size = new System.Drawing.Size(109, 16);
+            this.lblPrecoVenda.TabIndex = 10;
+            this.lblPrecoVenda.Text = "Preço de Venda:";
+            // 
+            // txtLucro
+            // 
+            this.txtLucro.Location = new System.Drawing.Point(542, 65);
+            this.txtLucro.Name = "txtLucro";
+            this.txtLucro.Size = new System.Drawing.Size(128, 20);
+            this.txtLucro.TabIndex = 13;
+            // 
+            // lblLucro
+            // 
+            this.lblLucro.AutoSize = true;
+            this.lblLucro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLucro.Location = new System.Drawing.Point(492, 66);
+            this.lblLucro.Name = "lblLucro";
+            this.lblLucro.Size = new System.Drawing.Size(44, 16);
+            this.lblLucro.TabIndex = 12;
+            this.lblLucro.Text = "Lucro:";
             // 
             // FrmCadastroProdutos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(688, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLucro);
+            this.Controls.Add(this.lblLucro);
+            this.Controls.Add(this.txtPrecoVenda);
+            this.Controls.Add(this.lblPrecoVenda);
+            this.Controls.Add(this.txtPrecoCusto);
+            this.Controls.Add(this.lblPrecoCusto);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblFornecedor);
+            this.Controls.Add(this.txtCodigoBarras);
+            this.Controls.Add(this.lblCodBarras);
+            this.Controls.Add(this.txtNomeProduto);
+            this.Controls.Add(this.lblNomeProduto);
             this.Controls.Add(this.txtCodProduto);
             this.Controls.Add(this.lblCodProduto);
             this.Name = "FrmCadastroProdutos";
@@ -91,7 +198,17 @@
 
         private System.Windows.Forms.Label lblCodProduto;
         private System.Windows.Forms.TextBox txtCodProduto;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblNomeProduto;
+        private System.Windows.Forms.TextBox txtNomeProduto;
+        private System.Windows.Forms.TextBox txtCodigoBarras;
+        private System.Windows.Forms.Label lblCodBarras;
+        private System.Windows.Forms.Label lblFornecedor;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblPrecoCusto;
+        private System.Windows.Forms.TextBox txtPrecoCusto;
+        private System.Windows.Forms.TextBox txtPrecoVenda;
+        private System.Windows.Forms.Label lblPrecoVenda;
+        private System.Windows.Forms.TextBox txtLucro;
+        private System.Windows.Forms.Label lblLucro;
     }
 }
