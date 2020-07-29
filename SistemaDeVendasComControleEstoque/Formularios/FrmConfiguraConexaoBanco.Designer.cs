68a1c43fc99142474f7849ea5013fa866fc9c6ad
Fábio Aragao <fabio_aragao@yahoo.com.br>
2020-07-29 02:30:18 -0300

    Formulario cadastro banco dados

---
+++ SistemaDeVendasComControleEstoque/Formularios/FrmConfiguraConexaoBanco.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace SistemaDeVendasComControleEstoque.Formularios
+{
+    partial class FrmConfiguraConexaoBanco
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnderecoServidor = new System.Windows.Forms.Label();
+            this.txtEnderecoServidor = new System.Windows.Forms.TextBox();
+            this.txtPortaServidor = new System.Windows.Forms.TextBox();
+            this.lblPorta = new System.Windows.Forms.Label();
+            this.txtUsuarioBanco = new System.Windows.Forms.TextBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtSenhaBanco = new System.Windows.Forms.TextBox();
+            this.lblSenhaBanco = new System.Windows.Forms.Label();
+            this.txtNomeBanco = new System.Windows.Forms.TextBox();
+            this.lblNomeBanco = new System.Windows.Forms.Label();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEnderecoServidor
+            // 
+            this.lblEnderecoServidor.AutoSize = true;
+            this.lblEnderecoServidor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnderecoServidor.Location = new System.Drawing.Point(12, 9);
+            this.lblEnderecoServidor.Name = "lblEnderecoServidor";
+            this.lblEnderecoServidor.Size = new System.Drawing.Size(124, 16);
+            this.lblEnderecoServidor.TabIndex = 0;
+            this.lblEnderecoServidor.Text = "Endereço Servidor:";
+            // 
+            // txtEnderecoServidor
+            // 
+            this.txtEnderecoServidor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnderecoServidor.Location = new System.Drawing.Point(142, 6);
+            this.txtEnderecoServidor.Name = "txtEnderecoServidor";
+            this.txtEnderecoServidor.Size = new System.Drawing.Size(324, 22);
+            this.txtEnderecoServidor.TabIndex = 1;
+            // 
+            // txtPortaServidor
+            // 
+            this.txtPortaServidor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPortaServidor.Location = new System.Drawing.Point(142, 34);
+            this.txtPortaServidor.Name = "txtPortaServidor";
+            this.txtPortaServidor.Size = new System.Drawing.Size(324, 22);
+            this.txtPortaServidor.TabIndex = 3;
+            // 
+            // lblPorta
+            // 
+            this.lblPorta.AutoSize = true;
+            this.lblPorta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorta.Location = new System.Drawing.Point(12, 37);
+            this.lblPorta.Name = "lblPorta";
+            this.lblPorta.Size = new System.Drawing.Size(43, 16);
+            this.lblPorta.TabIndex = 2;
+            this.lblPorta.Text = "Porta:";
+            // 
+            // txtUsuarioBanco
+            // 
+            this.txtUsuarioBanco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioBanco.Location = new System.Drawing.Point(142, 62);
+            this.txtUsuarioBanco.Name = "txtUsuarioBanco";
+            this.txtUsuarioBanco.Size = new System.Drawing.Size(324, 22);
+            this.txtUsuarioBanco.TabIndex = 5;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(12, 65);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(58, 16);
+            this.lblUsuario.TabIndex = 4;
+            this.lblUsuario.Text = "Usuário:";
+            // 
+            // txtSenhaBanco
+            // 
+            this.txtSenhaBanco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenhaBanco.Location = new System.Drawing.Point(142, 90);
+            this.txtSenhaBanco.Name = "txtSenhaBanco";
+            this.txtSenhaBanco.Size = new System.Drawing.Size(324, 22);
+            this.txtSenhaBanco.TabIndex = 7;
+            // 
+            // lblSenhaBanco
+            // 
+            this.lblSenhaBanco.AutoSize = true;
+            this.lblSenhaBanco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenhaBanco.Location = new System.Drawing.Point(12, 93);
+            this.lblSenhaBanco.Name = "lblSenhaBanco";
+            this.lblSenhaBanco.Size = new System.Drawing.Size(50, 16);
+            this.lblSenhaBanco.TabIndex = 6;
+            this.lblSenhaBanco.Text = "Senha:";
+            // 
+            // txtNomeBanco
+            // 
+            this.txtNomeBanco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeBanco.Location = new System.Drawing.Point(142, 118);
+            this.txtNomeBanco.Name = "txtNomeBanco";
+            this.txtNomeBanco.Size = new System.Drawing.Size(324, 22);
+            this.txtNomeBanco.TabIndex = 9;
+            // 
+            // lblNomeBanco
+            // 
+            this.lblNomeBanco.AutoSize = true;
+            this.lblNomeBanco.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeBanco.Location = new System.Drawing.Point(12, 121);
+            this.lblNomeBanco.Name = "lblNomeBanco";
+            this.lblNomeBanco.Size = new System.Drawing.Size(109, 16);
+            this.lblNomeBanco.TabIndex = 8;
+            this.lblNomeBanco.Text = "Nome do Banco:";
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravar.Location = new System.Drawing.Point(181, 157);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(112, 49);
+            this.btnGravar.TabIndex = 10;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // FrmConfiguraConexaoBanco
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 218);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.txtNomeBanco);
+            this.Controls.Add(this.lblNomeBanco);
+            this.Controls.Add(this.txtSenhaBanco);
+            this.Controls.Add(this.lblSenhaBanco);
+            this.Controls.Add(this.txtUsuarioBanco);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.txtPortaServidor);
+            this.Controls.Add(this.lblPorta);
+            this.Controls.Add(this.txtEnderecoServidor);
+            this.Controls.Add(this.lblEnderecoServidor);
+            this.Name = "FrmConfiguraConexaoBanco";
+            this.Text = "Configura Conexão Banco de Dados:";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnderecoServidor;
+        private System.Windows.Forms.TextBox txtEnderecoServidor;
+        private System.Windows.Forms.TextBox txtPortaServidor;
+        private System.Windows.Forms.Label lblPorta;
+        private System.Windows.Forms.TextBox txtUsuarioBanco;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtSenhaBanco;
+        private System.Windows.Forms.Label lblSenhaBanco;
+        private System.Windows.Forms.TextBox txtNomeBanco;
+        private System.Windows.Forms.Label lblNomeBanco;
+        private System.Windows.Forms.Button btnGravar;
+    }
+}
